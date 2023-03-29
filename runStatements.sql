f385b1ac78079c42b21a1e57e18a393d5c871f95
BenjaminGonzalezH <56660305+BenjaminGonzalezH@users.noreply.github.com>
2023-03-29 19:24:33 -0300

    Sentencias 5 y 6 de enunciado

--- runStatements.sql
+++ runStatements.sql
@@ -15,4 +15,19 @@ where
 	Ce_Vo.id_cliente = Ce.id_cliente and
 	Sn.id_seccion = Pe.id_seccion and
 	Ce.nacionalidad = 'Argentina'
-group by Sn.id_seccion
+group by Sn.id_seccion;
+
+SELECT s.fecha_inscrito,e.id_empleado,e.rol,MAX(s.cantidad) AS vuelos
+FROM Sueldo s
+INNER JOIN Empleado e ON e.id_sueldo=s.id_sueldo
+WHERE s.fecha_inscrito >= '2020-04-03' AND s.fecha_inscrito < '2023-04-03'
+	AND e.rol='Piloto'
+GROUP BY s.fecha_inscrito,e.id_empleado,e.rol,s.cantidad
+ORDER BY s.fecha_inscrito DESC;
+
+SELECT a.id_avion, COUNT(a.id_avion) AS vuelos
+FROM Vuelo v
+INNER JOIN Compania c ON c.id_compania=v.id_compania
+INNER JOIN Avion a ON c.id_avion=a.id_avion
+GROUP BY a.id_avion
+ORDER BY vuelos ASC LIMIT 1
